623f9869d107d3465aefb978f0ea7f2a11b89f91
ruxuebu <rbu@stevens.edu>
2017-02-24 00:48:33 -0500

    Add Summer Project

---
+++ app/scripts/loadviews.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  console.log("document ready!");
+  $("#header").load("views/header.html"); 
+  //$("#footer").load("views/footer.html");
+  $('#welcome').load('views/welcome.html');
+  $('#inbox').load('views/inbox.html');
+  $('#personal').load('views/personal.html'); 
+  $('#promotion').load('views/promotion.html'); 
+  $('#trash').load('views/trash.html');
+  $('#stevens').load('views/stevens.html');
+  
+  $('#querymodal').load('views/querymodal.html');
+  $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled").promise().done(function(){
+            console.log("toggled wraper");
+        }) // from remove to add OR from add to remove this toggled class
+  });
+  // console.log($('#welcome-footer'));
+});
